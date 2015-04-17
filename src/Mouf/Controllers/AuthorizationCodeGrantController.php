32a397faf2d36c4ed5fc057f66a64754ede33a52
Romain Marrot <r.marrot@thecodingmachine.com>
2015-04-17 11:32:11 +0200

    Create controller AuthorizationCodeGrantController

---
+++ src/Mouf/Controllers/AuthorizationCodeGrantController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Mouf\Controllers;
+
+use Mouf\Mvc\Splash\Controllers\Controller;
+
+/**
+ * TODO: write controller comment
+ */
+class AuthorizationCodeGrantController extends Controller {
+
+    /**
+     * Controller's constructor.
+     */
+    public function __construct() {
+    }
+
+    /**
+     * @URL /
+     */
+    public function index() {
+        return null;
+    }
+}
--- src/Mouf/OAuthServer/OAuthServerInstaller.php
+++ src/Mouf/OAuthServer/OAuthServerInstaller.php
@@ -23,6 +23,7 @@ class OAuthServerInstaller implements PackageInstallerInterface {
         $entityManager = $moufManager->getInstanceDescriptor('entityManager');
 
         // Let's create the instances.
+        InstallUtils::getOrCreateInstance('authorizationCodeGrantController', 'Mouf\\Controllers\\AuthorizationCodeGrantController', $moufManager);
         $accessTokenRepository = InstallUtils::getOrCreateInstance('accessTokenRepository', 'Mouf\\OAuthServer\\Model\\Entities\\AccessTokenRepository', $moufManager);
         $authCodeRepository = InstallUtils::getOrCreateInstance('authCodeRepository', 'Mouf\\OAuthServer\\Model\\Entities\\AuthCodeRepository', $moufManager);
         $clientRepository = InstallUtils::getOrCreateInstance('clientRepository', 'Mouf\\OAuthServer\\Model\\Entities\\ClientRepository', $moufManager);
